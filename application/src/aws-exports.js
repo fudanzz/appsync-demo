1d1778dbe76207f9e723ea2c225192312492493a
phil zhao <fudanzz@yahoo.com>
2021-06-08 21:01:39 +0800

    first commit

---
+++ application/src/aws-exports.js
@@ -0,0 +1,10 @@
+const awsmobile = {
+  aws_appsync_graphqlEndpoint: 'https://hhcyovelebgipjn7kse5uwa7me.appsync-api.us-east-1.amazonaws.com/graphql',
+  aws_appsync_authenticationType: 'AMAZON_COGNITO_USER_POOLS',
+  aws_appsync_region: 'us-east-1',
+  aws_cognito_region: 'us-east-1',
+  aws_user_pools_id: 'us-east-1_ewtVsrorq',
+  aws_user_pools_web_client_id: '1iirpql7ug6a361t13bikb3cbo',
+}
+
+export default awsmobile
